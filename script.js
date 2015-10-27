2767df15dfc9af3bd40f8a916e5a600325fb4bf3
Eric Stennett <estennett@gmail.com>
2015-10-26 22:20:57 -0400

    Finished adding in the questions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var game = {
     answeredCorrect : 0,
     answeredWrong : 0,
     whichCard : 1,
-    deckSize: 8,
+    deckSize: 20,
 
     //setting decksize below
     // deckSizeFunction : function(obj) {
@@ -30,7 +30,7 @@ var game = {
         $('#buttons').html("It is over");
       }else{
               var cards = this.allCards["card"+this.whichCard];
-              $('#questions').html(cards.question);
+              $('#questions').html('<span>' + cards.question + '</span>');
               $('#0').html(cards.option[0]);
               $('#1').html(cards.option[1]);
               $('#2').html(cards.option[2]);
@@ -106,15 +106,77 @@ var game = {
         option : ["September 15th", "August 5th", "September 27th", "August 13th", "September 23rd"],
       },
       card7 : {
-        question: "Work dammit?",
-        answer : "Learning",
-        option : ["Insanity", "Triginometry", "Start of human extinction", "Learning", "No correct answer"],
+        question: "What is a Honey Badger's favorite food?",
+        answer : "Honey",
+        option : ["Honey", "Humans", "Ants", "Beer", "Math"],
       },
       card8 : {
-        question: "In the popular TV program Adventure Time, what is the name of the cranky despot styled afer a lemon?",
+        question: "Who played Sirius Black in the Harry Potter feature film franchise?",
         answer : "Lemongrab",
-        option : ["Princess Bubblegum", "Jake", "Lemonhurt", "Lemongrab", "Peppermint Butler"],
+        option : ["Jude Law", "Elijah Wood", "Benedict Cumberbatch", "Gary Oldman", "Steve Buscemi"],
       },
+      card9 : {
+        question: "The Karni Mata temple in India is dedicated to which furry animal?",
+        answer : "Rats",
+        option : ["Koalas", "Cows", "Red Pandas", "Rats", "Gerard Butler"],
+      },
+      card10 : {
+        question: "One of the most complex parts of a watch is known as a ",
+        answer : "Tourbillon",
+        option : ["Barrel Spring", "Bregeut Slip Spring", "Parachrom Hairspring", "Synthetic Ruby", "Tourbillon"],
+      },
+      card11 : {
+        question: "What would happn if you pass the event horizon?",
+        answer : "No one knows",
+        option : ["No one knows", "Spaghettification", "You attain Nirvana", "Immediate death", "Alien contact"],
+      },
+      card12 : {
+        question: "Out of the options below, who is the hottest President of the United States of America?",
+        answer : "Slick Willy",
+        option : ["W", "Slick Willy", "Calivn Coolidge", "Vladamir Putin", "Obama for 2016"],
+      },
+      card13 : {
+        question: "Why do cats blink slowly at you?",
+        answer : "A sign of purrr love",
+        option : ["A sign of purrr hate", "Jealousy", "Intoxication", "Anger", "A sign of purrr love"],
+      },
+      card14 : {
+        question: "How much wood could a woodchuck chuck if a woodchuck could chuck wood?",
+        answer : "60 lols",
+        option : ["50 cords", "20 cords", "10 cords", "30 cords", "60 lols"],
+      },
+      card15 : {
+        question: "What was the first computer called?",
+        answer : "ENIAC",
+        option : ["ENIAC", "SKYNET", "MNTDOOM", "DARPA", "Antikythera mechanism"],
+      },
+      card16 : {
+        question: "Velociraptor takes you by surprise, you say:",
+        answer : "Clever girl",
+        option : ["&!@#", "....", "Ellie, the doorlocks!", "Clever girl", "Must go faster"],
+      },
+      card17 : {
+        question: "What color is #FF0000",
+        answer : "Red",
+        option : ["Yellow", "Purple Mountain Majesty", "Red", "Periwinkle", "Grey"],
+      },
+      card18 : {
+        question: "Although lesser known, this style of Buddhism focuses on immediate enlightnement.",
+        answer : "Vajrayana",
+        option : ["Theravada", "Mahayana", "Jodo Shinshu", "Vajrayana", "Hinduyana"],
+      },
+      card19 : {
+        question: "Who is the man/woman?",
+        answer : "You are!",
+        option : ["Jack Sparrow", "You are!", "Mr. Bond", "Dr. Grant", "Nick Cage"],
+      },
+      card20 : {
+        question: "Where is the highest swamp in the world?",
+        answer : "Kauai, Hawaii",
+        option : ["Kauai, Hawaii", "Mordor", "England", "Colombia", "Colorado"],
+      },
+
+
   },
 }
 
